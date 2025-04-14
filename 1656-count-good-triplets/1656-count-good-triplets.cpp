493d625a7ebd13d3e351bbf7da5fbd47a40e5c2f
Harsh Vardhan <57543613+The-Harsh-Vardhan@users.noreply.github.com>
2025-04-14 23:09:55 +0530

    Time: 8 ms (51.06%), Space: 11 MB (71.62%) - LeetHub

--- 1656-count-good-triplets/1656-count-good-triplets.cpp
+++ 1656-count-good-triplets/1656-count-good-triplets.cpp
@@ -5,10 +5,8 @@ public:
         int count = 0;
         for( int i = 0 ; i < n ; i++ ) {
             for( int j = i + 1 ; j < n ; j++ ) {
-                    for( int k = j + 1 ; k < n ; k++ ) {
-                        if( abs(arr[i] - arr[j]) <= a && abs(arr[j] - arr[k]) <= b && abs(arr[i] - arr[k]) <= c ) {
-                            count++;
-                        }
+                for( int k = j + 1 ; k < n ; k++ ) {
+                    if( abs(arr[i] - arr[j]) <= a && abs(arr[j] - arr[k]) <= b && abs(arr[i] - arr[k]) <= c ) count++;
                 }
             }
         }
