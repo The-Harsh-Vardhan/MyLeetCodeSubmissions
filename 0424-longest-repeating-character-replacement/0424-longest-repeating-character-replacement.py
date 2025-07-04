fdd465e7cb98c17450c25c978b8ceb466dbbff94
Harsh Vardhan <57543613+The-Harsh-Vardhan@users.noreply.github.com>
2025-07-04 23:45:40 +0530

    Time: 173 ms (33.32%), Space: 18.2 MB (23.6%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,8 +1,15 @@
-class Solution(object):
-    def characterReplacement(self, s, k):
-        """
-        :type s: str
-        :type k: int
-        :rtype: int
-        """
-        
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        count = {} #declaring hashset to count the number of occurances of each characters
+        result = 0 #length of the longest substring
+
+        l = 0 #declaring left pointer to 0
+        for r in range(len(s)):
+            count[s[r]] = 1 + count.get(s[r],0) #increasing the count of the character if does not exist then default value 0
+
+            while (r - l + 1) - max(count.values()) > k: #lenght of the windows - count of the most frequent character > no. of replacements allowed
+                count[s[l]] -= 1 #taking count of character at the left position and decrementing it by 1
+                l += 1 #increasing the value of the left pointer if 
+
+            result = max(result, r - l + 1) #size of the window
+        return result
